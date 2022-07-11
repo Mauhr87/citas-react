8b960af6dee5becf4ee1f8e884196cecd5d29854
Mauricio Hidalgo <mhidalgodg@gmail.com>
2022-07-11 13:30:58 -0600

    setup tailwindcss and app start layout

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListadoPacientes() {
+  return (
+    <div className="w-1/2 lg:2/5">
+        <h2>Pacientes en un H2</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
